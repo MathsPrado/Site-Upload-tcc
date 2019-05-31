5f24f82ce8331a3a246c6fdd9f8e9954be27510e
Matheus <matheus.csharp@hotmail.com>
2019-05-31 18:31:37 -0300

    Agora esta recebendo os pdfs

--- recebe_upload.php
+++ recebe_upload.php
@@ -1,22 +1,27 @@
 <?php
- 
-$diretorio_arq = 'C:\Users\Isaias\Documents';
-$arq = $_FILES['arquivo']; 
- 
-$nome_arq    = $arq['name']; 
-$tamanho_arq = $arq['size']; 
-$tipo_arq    = $arq['type']; 
-$tmpname_arq = $arq['tmp_name']; 
+if(isset($_FILES['file'])){
 
-if($tamanho_arq > 0 && strlen($nome_arq) > 1) {  
-    $caminho_arq = $diretorio_arq . $nome_arq; 
-         
-	if(move_uploaded_file($tmpname_arq, $diretorio_arq)){ 
-		echo"Cadastro realizado com sucesso";				
-	}else{
-	    echo 'Não foi possível enviar'; 
-		} 
+	$arq = $_FILES['file']; 
+	//Dados do arquivo
+	$nome_arq    = $arq['name']; 
+	$tmpname_arq = $arq['tmp_name']; 
+
+	$arq_ext = explode('.', $nome_arq);
+	$arq_ext = strtolower(end($arq_ext));
+
+	$tipo_aceitos = array('pdf', 'doc');
+
+	if(in_array($arq_ext, $tipo_aceitos)) {  
+		$arq_destino = 'uploads/' . $nome_arq;		
+
+		if(move_uploaded_file($tmpname_arq, $arq_destino)){ 
+			echo"Cadastro realizado com sucesso";
+			echo $arq_destino;				
+		}else{
+			echo 'Não foi possível enviar'; 
+			} 
+	}
 }else{
-    echo"Erro";
-} 
+	echo"ERRO";
+}
 ?>
