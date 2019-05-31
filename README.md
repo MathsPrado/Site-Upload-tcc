a3f5f0e708c04addbe7079f7d8043e3c36da881b
Matheus <matheus.scharp@hotmail.com>
2019-05-31 12:22:25 -0300

    minha primeira alteração no codigo original

---
+++ README.md
@@ -0,0 +1,2 @@
+# Site-Upload-tcc
+trabalho php Sistema para subir seu tcc. Os alunos entregam seu tcc e o sistema vai cadastrar o tcc, quem é o aluno e seu curso, Ocorrendo também palavras chaves para pesquisa dos tccs já cadastrado (Salvo no banco). Buscando na tabela de cursos os resumos.
--- cadastro.php
+++ cadastro.php
@@ -2,8 +2,6 @@
 
 include ('conexao.php');
 
-
-
 $nome = $_POST['user_name'];
 
 $login = $_POST['user_login'];
@@ -15,9 +13,8 @@ $curso = $_POST['user_curso'];
 $email = $_POST['user_email'];
 
 
-
-
-$in = mysqli_query($conexao,"insert into usuarios (NOME, CURSO, LOGIN, SENHA, EMAIL)values ('$nome','$curso','$login','$senha','$email')");
+$in = mysqli_query($conexao,"insert into usuarios (NOME, CURSO, LOGIN, SENHA, EMAIL)
+    values ('$nome','$curso','$login','$senha','$email')");
 
 header("location: login.php");
 
