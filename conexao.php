c5838f1bad0b00961313c8d37f90e9f016b13647
Matheus <matheus.csharp@hotmail.com>
2019-06-04 23:42:14 -0300

    criei uma nova funcao para validar

--- conexao.php
+++ conexao.php
@@ -13,7 +13,21 @@ function novoUsuario($con,$nome,$senha,$curso,$email){
     mysqli_close($con);
 }
 
-mysqli_query($con,"SELECT * FROM usuarios");
+function validaUsuario($con,$email,$senha){
+    $sql = mysqli_query($con,"SELECT `email`,`senha` FROM `usuarios` WHERE `email`='" . $email. "' AND `senha`='" . $senha . "' limit 2");
+    if (mysqli_num_rows($sql) == 1) {
+        session_start();
+        while ($row = mysqli_fetch_assoc($sql)) {
+            $_SESSION['sessao_user'] = $row['email'];
+            $_SESSION['sessao_senha'] = $row['senha'];
+        }
+        header("Location: pagina.php");
+    } else {
+        echo "conta inválida";
+    }
+}
+
+
 
     
 ?>
